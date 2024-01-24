8025ba2a5193792568d4263e1fc75d451fd33e92
iand-itb <ian.diaz.7e5@itb.cat>
2024-01-24 11:46:29 +0100

    e2 finikilao

--- e2.py
+++ e2.py
@@ -6,3 +6,16 @@ nombres que es troben a les posicions parelles i la mitja del nombre de les posi
 '''
 import random
 
+LIMITINF, LIMITSUP = 1, 50
+lista = [random.randint(LIMITINF, LIMITSUP) for i in range(100)]
+totalParells, totalSenars = 0, 0
+
+for i in range(len(lista)):
+    if i % 2 == 0:
+        totalParells += lista[i]
+    else:
+        totalSenars += lista[i]
+
+mitjanaParells = totalParells / (len(lista) / 2)
+mitjanaSenars = totalSenars / (len(lista) / 2)
+print(f'Mitjana de les posicions parelles: {mitjanaParells}\nMitjana de les posicions senars: {mitjanaSenars}')
