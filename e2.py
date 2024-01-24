63ea9bbbc3126bdcb971256597b9af48547b5c88
iand-itb <ian.diaz.7e5@itb.cat>
2024-01-24 11:28:19 +0100

    ..

---
+++ e2.py
@@ -0,0 +1,8 @@
+'''
+24/1/2024
+Ayman Dghoughi, Nizar ElKhoulfi, Ian Díaz
+e2: Programa que generi una llista de 100 nombres aleatoris entre 1 i 50. Obtenir la mitja dels
+nombres que es troben a les posicions parelles i la mitja del nombre de les posicions senars.
+'''
+import random
+
