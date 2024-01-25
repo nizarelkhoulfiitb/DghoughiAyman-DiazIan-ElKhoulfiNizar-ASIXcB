2402d0644e9223e418fa3221d2121320f21fd028
iand-itb <ian.diaz.7e5@itb.cat>
2024-01-25 12:31:49 +0100

    final

--- e3.py
+++ e3.py
@@ -6,25 +6,25 @@ Programa de traducció d’insults. Crear una estructura de dues dimensions amb 
 El programa, demanarà a l’usuari que escrigui per teclat un insult, en català, i el mostrarà traduït a castellà, anglès i klingon.
 """
 exists = False
-insults = {'CAT': ('CARALLOT', 'MOCÓS', 'MALPARIT', 'TROS DE QUÒNIAM', 'FLASCA', 'TARAL·LIROT', 'LLONDRO', 'ABRAÇAFANALS', 'AFAITAPAGESOS', 'BORDEGÀS'),
+INSULTS = {'CAT': ('CARALLOT', 'MOCÓS', 'MALPARIT', 'TROS DE QUÒNIAM', 'FLASCA', 'TARAL·LIROT', 'LLONDRO', 'ABRAÇAFANALS', 'AFAITAPAGESOS', 'BORDEGÀS'),
            'ESP': ('CARALLO', 'MOCOS', 'MALPARIDO', 'TROZO DE CARNE', 'FLEMA', 'TARAL·LIROT', 'TONTOLABA', 'ARRASTRADO', 'ADULADOR', 'APROVECHADO'),
            'ENG': ('JERK', 'SNOT', 'SCOUNDREL', 'BLOCKHEAD', 'PHLEGM', 'FOOL', 'DUNDERHEAD', 'GROVELER', 'SUCK-UP', 'EXPLOITER'),
            'KLI': ('KJERK', 'KSNOT', 'KSCOUNDREL', 'KBLOCKHEAD', 'KPHLEGM', 'KFOOL', 'KDUNDERHEAD', 'KGROVELER', 'KSUCK-UP', 'KEXPLOITER')}
 insult = input().upper().replace(" ", "")
 idiomes = ['CAT', 'ESP', 'ENG', 'KLI']
 # Ejercicio no bonus:
-for i in range(len(insults["CAT"])):
-    if insults['CAT'][i] == insult:
-        print(f'Espanyol:\t{insults["ESP"][i]}\nAnglès:\t\t{insults["ENG"][i]}\nKlingon:\t{insults["KLI"][i]}')
+for i in range(len(INSULTS["CAT"])):
+    if INSULTS['CAT'][i] == insult:
+        print(f'Espanyol:\t{INSULTS["ESP"][i]}\nAnglès:\t\t{INSULTS["ENG"][i]}\nKlingon:\t{INSULTS["KLI"][i]}')
 
 #Ejercicio bonus:
 for x in idiomes:
-    for i in range(len(insults[x])):
-        if insults[x][i] == insult:
-            print(f'idioma escollit: {x}, insult: {insults[x][i]}')
+    for i in range(len(INSULTS[x])):
+        if INSULTS[x][i] == insult:
+            print(f'idioma escollit: {x}, insult: {INSULTS[x][i]}')
             idiomes.remove(x)
             for x in idiomes:
-                print(f'{x}: {insults[x][i]}')
+                print(f'{x}: {INSULTS[x][i]}')
             exists = True
 if exists == False:
     print("El insulto no existe en la lista.")
